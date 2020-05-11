f4eb399acd5a0a2d5ba56382c1ac425499c1dcaa
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-05-11 09:57:33 +0100

    changed js

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 
 let color = document.querySelector('#colorPicker');
 let table = document.querySelector('#pixelCanvas');
-let sizePicker = document.querySelector('#sizePicker');
+let sizePicker = document.querySelector('#submit');
 
 let height = document.querySelector('#inputHeight').Value;
 let width = document.querySelector('#inputWidth').value;
 makeGrid(height, width);
-
+let initial = 0;
 
 sizePicker.addEventListener("click", (event) => {
     event.preventDefault();
     let height = document.querySelector('#inputHeight').value;
+    console.log(height)
     let width = document.querySelector('#inputWidth').value;
-    table.firstElementChild.remove();
+    console.log(width)
+    if(initial!==0){
+        table.firstElementChild.remove();
+        
+    }
+   
 
     makeGrid(height, width);
+    initial++
 });
 
 function makeGrid(height, width) {
     for( let i = 0; i < height; i++) {
         let row = table.insertRow(i);
+        
         for( let j = 0; j < width; j++) {
             let cell = row.insertCell(j);
-            cell.addEventListener("click", (event) => {
+            
+       cell.addEventListener("mousemove", (event) => {
                 cell.style.backgroundColor = color.value;
             })
         }
